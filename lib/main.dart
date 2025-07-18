c3f9f5699c14b7bf2a4b31490dfce2122fdaa52e
tnmrks <tnmrks1@gmail.com>
2025-07-18 10:08:23 +0700

    feat: connect flutter app to firebase and fix build issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart'; // Kita akan membuat file ini selanjutnya
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Inisialisasi Firebase akan sedikit berbeda tanpa flutterfire_cli
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
